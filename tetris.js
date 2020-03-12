e4bc11a3051bfbfa24bc198fb1fa1cd8696d8c59
Syow <crescentsyou@gmail.com>
2020-03-12 20:40:38 +0800

    draw ,merge

--- tetris.js
+++ tetris.js
@@ -12,6 +12,13 @@ context.strokeStyle = "#000" ;
 context.strokeRect(0,0,board.width ,board.height) ; 
 //context.scale(30,30);
 
+let Block = []
+for(let i = 0 ; i < 20; i++){
+    Block.push([]) ;
+    for(let j = 0 ; j < 10 ; j++){
+        Block[i].push(0);
+    }
+}
 
 function createMatrix(){
     return [
@@ -26,25 +33,16 @@ let player = {
     matrix : createMatrix() 
 };
 
+
 function DrawMatrix(matrix,pos){
     matrix.forEach((row,y) => {
         row.forEach((col,x) => {
             if(matrix[y][x] !== 0){
                 context.fillStyle = "#a0a" ;
                 context.fillRect((pos.x+x)*scale,(pos.y+y)*scale,1*scale,1*scale);
+                context.strokeStyle = "#fff" ;
+                context.strokeRect((pos.x+x)*scale,(pos.y+y)*scale,1*scale,1*scale);
                 context.strokeStyle = "#c6c" ;
-                context.strokeRect((pos.x+x)*scale,board.height-(matrix.length-1-y)*scale,1*scale,1*scale);
-            }
-        });
-    });
-}
-function ClearMatrix(matrix,pos){
-    matrix.forEach((row,y) => {
-        row.forEach((col,x) => {
-            if(matrix[y][x] !== 0){
-                context.fillStyle = "#fff" ;
-                context.fillRect((pos.x+x)*scale,(pos.y+y)*scale,1*scale,1*scale);
-                context.strokeStyle = "#000" ;
                 context.strokeRect((pos.x+x)*scale,board.height-(matrix.length-1-y)*scale,1*scale,1*scale);
             }
         });
@@ -53,19 +51,46 @@ function ClearMatrix(matrix,pos){
 
 function Drop(){
     player.pos.y++;
+    if( checkCross(player.matrix,player.pos) ){
+        player.pos.y--;
+        merge(player.matrix,player.pos) ;
+    }
     setTimeout(Drop,droptime);
+}
+
+function merge(matrix,pos){
+    matrix.forEach((row,x)=>{
+        row.forEach((col,y)=>{
+            if( matrix[y][x] !== 0 ){
+                Block[y+pos.y][x+pos.x] = matrix[y][x] ;
+            }
+        })
+    })
+}
+
+function checkCross(matrix,pos){
+    for(let y = 0 ; y < matrix.length ; y++){
+        for(let x = 0 ; x < matrix[y].length ; x++){
+            if( matrix[y][x] !== 0 &&
+                (Block[ y+pos.y ] &&
+                 Block[ y+pos.y][ x+pos.x]) !==0 )
+                return true ;
+        }
+    }
+    
+    return false ;
 }
 
 function update(time = 0){
     
-    //console.log(time);
-    /*let lastTime = time ;
-    if( time - lastTime> droptime ){
-        player.pos.y++ ;
-        lastTime = time ;
-    }*/
-    
+    context.fillStyle = "#fff" ;
+    context.fillRect(0,0,board.width ,board.height) ;
+    context.lineWidth = "2" ;
+    context.strokeStyle = "#000" ;
+    context.strokeRect(0,0,board.width ,board.height) ;  
+
     DrawMatrix(player.matrix,{x :player.pos.x ,y: player.pos.y} ) ;
+
     //requestAnimationFrame(update());
 }
 
@@ -74,7 +99,35 @@ function TetrisStart(){
     update();
     
 }
-//DrawMatrix(player.matrix,player.pos) ;
+
+document.addEventListener('keydown',function(){
+    switch(event.code){
+        case 'ArrowLeft'    : ChangeX( -1 ) ;  break;
+        case 'ArrowRight'   : ChangeX( 1 ) ; break;
+        case 'ArrowDown'    : ChangeY( 1 );  break;
+        case 'Space'        : GoBottom() ; break;
+        case 'KeyZ'         : console.log('z');     break;
+        case 'KeyX'         : console.log('x');     break;
+        case 'KeyC'         : console.log('c');     break;
+    }
+});
+
+function ChangeX(x){
+    player.pos.x += x ;
+    if(checkCross(player.matrix,player.pos)){
+        player.pos.x -= x ;
+    }
+}
+function ChangeY(y){
+    player.pos.y += y ;
+    if(checkCross(player.matrix,player.pos)){
+        player.pos.y -= y ;
+    }
+}
+function GoBottom(){
+    player.pos.y = board.height / scale + 1 - player.matrix.length ;
+}
+
 
 setInterval(update,10);
 setTimeout(Drop,droptime);
